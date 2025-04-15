8842f75ed65e6b6d78277ab06009755196336e65
Frenmy Garcia <frenmymanuel@gmail.com>
2025-04-16 00:46:46 +0200

    � Actualizaciones en el servidor: nuevas rutas, mejoras en publicaciones y notificaciones

--- app.js
+++ app.js
@@ -8,9 +8,9 @@ import { v2 as cloudinary } from 'cloudinary';
 
 
 cloudinary.config({
-  cloud_name: CLOUD_NAME,
-  api_key: API_KEY,
-  api_secret: API_SECRET,
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.API_KEY,
+  api_secret: process.env.API_SECRET,
 });
 
 const app = express();
